51e3d980d173035748e364414f1d6399a1ba13ab
Juan Alberdi <153318090+1zaia@users.noreply.github.com>
2024-09-04 13:46:15 +0200

    aula 04/09

---
+++ year 1/Java/main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a);
+        
+    }
+
+}
