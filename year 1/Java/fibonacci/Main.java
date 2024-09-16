7cb152c6f4effaa89dcf3502bf6c26cf899c8beb
Juan Alberdi <jzalberdi@gmail.com>
2024-09-16 22:17:18 +0200

    a

--- year 1/Java/fibonacci/Main.java
+++ year 1/Java/fibonacci/Main.java
@@ -8,31 +8,33 @@ public class Main{
         int MaxArray = 10000000;
                 
         System.out.println("Escreva uma posicao de fibonacci: ");
-        Scanner scan = new Scanner(System.in);
-        posicao = scan.nextInt();
-        
+        try (Scanner scan = new Scanner(System.in)) {
+            posicao = scan.nextInt();
+        }
         int[] fibonacci = new int[MaxArray];
 
         fibonacci[0] = 0;
         fibonacci[1] = 1;
 
-        if(posicao == 0){
-            System.out.println("-- Sequencia de Fibonacci -- ");
-        }else if(posicao == 1){
-            System.out.println("-- Sequencia de Fibonacci -- ");
-            System.out.println(fibonacci[0]);
-        }else{
-            System.out.println("-- Sequencia de Fibonacci -- ");
-            System.out.print(fibonacci[0]);
-            System.out.print(" ");
-            System.out.print(fibonacci[1]);
-            System.out.print(" ");
-
-            for(int i = 2; i < posicao; i++){
-                fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
-                System.out.print(fibonacci[i]);
+        switch (posicao) {
+            case 0:
+                System.out.println("-- Sequencia de Fibonacci -- ");
+                break;
+            case 1:
+                System.out.println("-- Sequencia de Fibonacci -- ");
+                System.out.println(fibonacci[0]);
+                break;
+            default:
+                System.out.println("-- Sequencia de Fibonacci -- ");
+                System.out.print(fibonacci[0]);
                 System.out.print(" ");
-            }
+                System.out.print(fibonacci[1]);
+                System.out.print(" ");
+                for(int i = 2; i < posicao; i++){
+                    fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+                    System.out.print(fibonacci[i]);
+                    System.out.print(" ");
+                }   break;
         }
     
     }
